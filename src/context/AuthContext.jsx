d26646be80aa25a32769a9ea43efd50913a74b0d
Andydiii <Andydiii0813@gmail.com>
2025-04-11 20:53:24 -0400

    authentication should be good

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,9 +3,11 @@ import {
   signInWithEmailAndPassword,
   signOut,
   sendPasswordResetEmail,
+  onAuthStateChanged,
 } from "firebase/auth";
 import { useState, useEffect, useContext, createContext } from "react";
-import { auth } from "../../firebase";
+import { auth, db } from "../../firebase";
+import { doc, getDoc } from "firebase/firestore";
 
 const AuthContext = createContext();
 
@@ -14,8 +16,9 @@ export function useAuth() {
   return useContext(AuthContext);
 }
 
+// why dont we use all states in the context? bc less efficient
 export function AuthProvider({ children }) {
-  const [user, setUser] = useState(null);
+  const [globalUser, setGlobalUser] = useState(null);
   const [globalData, setGlobalData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -32,13 +35,57 @@ export function AuthProvider({ children }) {
   }
 
   function logout() {
-    setUser(null);
+    setGlobalUser(null);
     setGlobalData(null);
     return signOut(auth);
   }
 
-  // value in value prop is the global data
-  const value = { user, globalData, setGlobalData, isLoading };
+  useEffect(() => {
+    const unsubsribe = onAuthStateChanged(auth, async (user) => {
+      // if there is no user, empty the user state and return from this listener
+      if (!user) {
+        console.log("No user found");
+        return;
+      }
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+      // if there is a user, then check if the user has data in the database, and if they do, then fetch said data and update the global state.
+      try {
+        setIsLoading(true);
+
+        // fetch the user data from the database
+        // first we create a reference to the document (labelled json object), and then we get the doc, and then we snapshot it to see if there's anything there.
+        const docRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(docRef);
+
+        let firebaseData = {};
+        if (docSnap.exists()) {
+          console.log("Found user data");
+          firebaseData = docSnap.data();
+        }
+        setGlobalData(firebaseData);
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    });
+    return unsubsribe;
+  }, []); // dependency array determines when the useEffect runs, if empty, it runs once at the beginning
+
+  // value in value prop is the global data
+  const value = {
+    globalUser,
+    globalData,
+    setGlobalData,
+    isLoading,
+    signup,
+    login,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
